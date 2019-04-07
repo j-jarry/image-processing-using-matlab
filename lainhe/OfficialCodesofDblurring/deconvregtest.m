0dd03f6f7c1939e50f50dd1d721b9519fc924787
LainHE <1213793337@qq.com>
2019-04-07 23:31:48 +0100

    Add files via upload

---
+++ lainhe/OfficialCodesofDblurring/deconvregtest.m
@@ -0,0 +1,14 @@
+I = checkerboard(8);
+PSF = fspecial('gaussian',7,10);
+V = .01;
+BlurredNoisy = imnoise(imfilter(I,PSF),'gaussian',0,V);
+NOISEPOWER = V*prod(size(I));
+[J LAGRA] = deconvreg(BlurredNoisy,PSF,NOISEPOWER);
+subplot(221); imshow(BlurredNoisy);
+title('A = Blurred and Noisy');
+subplot(222); imshow(J);
+title('[J LAGRA] = deconvreg(A,PSF,NP)');
+subplot(223); imshow(deconvreg(BlurredNoisy,PSF,[],LAGRA/10));
+title('deconvreg(A,PSF,[],0.1*LAGRA)');
+subplot(224); imshow(deconvreg(BlurredNoisy,PSF,[],LAGRA*10));
+title('deconvreg(A,PSF,[],10*LAGRA)');
